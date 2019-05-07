5711c479eac3e289aa0e68dd1b42d8d4f43789d9
christianhelms <fonticons-dev@Fonticonss-MBP.hitronhub.home>
2019-05-07 17:24:18 -0500

    probs a plenty

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const dank = 'memes';
+function returnMyMemes () {
+  return dank;
+}
+returnMyMemes();
+
+console.log(returnMyMemes);
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
